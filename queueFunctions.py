04b0a2f59be6fdce442d47fca36f64abab5ec39d
Ru Esteban <rcesteban2003@gmail.com>
2024-09-10 05:40:17 -0400

    queueFunctions now has an alternate way of making queues This has not been implemented with buttons yet but theoretically should be functional

--- queueFunctions.py
+++ queueFunctions.py
@@ -1,89 +1,82 @@
 import random
+from queue import Queue
 
 
 class Fencer:
     def __init__(self, name, id):
         self.name = name
         self.id = id
-        self.fenced = []
+        self.bouts = 0
+        self.fenced = set()  # set of names
+        self.last = Fencer
+
+    def clear(self):
+        # Clears fenced list and re-adds last person fenced to keep repeat from happening
+        self.fenced.clear()
+        self.fenced.add(self.last)
 
 
-class Pair:
-    def __init__(self, one, two):
-        self.one = one
-        self.two = two
+class StripQueue:
+    # Currently pool is a dictionary with keys as number of bouts completed and values as sets of names of fencers
+    def __init__(self, strips):
+        self.strips = strips
+        self.pool = {0: set(), 1: set(), 2: set(), 3: set(), 4: set()}  # dictionary of sets
+        self.min_key = 0
+        self.num_fencers = 0
 
+    def init_pool(self, fencers):
+        # Add each person who voted yes to dictionary in set corresponding to 0 bouts completed
+        for fencer in fencers:
+            self.pool[0].add(fencer)
+            self.num_fencers += 1
 
-def check_pairs(first, second):
-    return not (first.one in {second.one, second.two} or first.two in {second.one, second.two})
+    def check_min(self):
+        # advances min set counter if set is empty
+        if not self.pool[self.min_key]:
+            self.min_key += 1
 
+    def add_to_pool(self, fencer):
+        # Add new fencer to dictionary in set corresponding to the 1 + the min number of bouts completed
+        self.check_min()
+        self.pool[self.min_key + 1].add(fencer)
 
-def reorder(pairs, fencers, max_unique_pairs):
-    queue = []
-    random.seed()
-    queue.append(pairs[0])
-    prev_pair = queue[0]
-    prev2_pair = Pair(0, 0)
+    def rnd_min_fencer(self):
+        # Return a random Fencer from the set corresponding to the min number of bouts completed
+        self.check_min()
+        min_list = list(self.pool[self.min_key])
+        return random.choice(min_list)
 
-    cycle = 0
-    refresh = 0
-    added_to_reorder = 1
+    def choose_pair(self, left=None, right=None):
+        # Returns a pair of Fencers
+        # If argument are not given, generate random Fencer(s)
+        if left is None:
+            left = self.rnd_min_fencer()
+        if right is None:
+            right = self.rnd_min_fencer()
+            while right == left or right.name in left.fenced:
+                # if fenced everyone already, clear the fenced list except for the last person fenced
+                if left.fenced.len() == self.num_fencers - 1:
+                    left.clear()
+                right = self.rnd_min_fencer()
+        return left, right
 
-    while added_to_reorder < max_unique_pairs:
-        if cycle > max_unique_pairs:
-            queue.clear()
-            queue.append(pairs[0])
-            prev_pair = queue[0]
-            added_to_reorder = 1
-            cycle = 0
-            refresh += 1
+    def skip_left(self, pair):
+        # Re-calculates pair while keeping the right Fencer
+        return self.choose_pair(pair[1])
 
-        rnd_pair_index = random.randint(0, max_unique_pairs - 1)
-        chosen = prev_pair
+    def skip_right(self, pair):
+        # Re-calculates pair while keeping the left Fencer
+        return self.choose_pair(pair[0])
 
-        while chosen == prev_pair or chosen in queue:
-            rnd_pair_index = random.randint(0, max_unique_pairs - 1)
-            chosen = pairs[rnd_pair_index]
-
-        check = True
-        if added_to_reorder > 1:
-            check = check_pairs(chosen, queue[added_to_reorder - 2])
-
-        if check_pairs(chosen, queue[added_to_reorder - 1]) and check:
-            queue.append(chosen)
-            prev2_pair = prev_pair
-            prev_pair = chosen
-            added_to_reorder += 1
-        else:
-            cycle += 1
-
-    print("refreshes:", refresh)
-    return [(fencers[pair.one].name, fencers[pair.two].name) for pair in queue]
-
-
-def main(names):
-    num_fencers = len(names)
-    fencers = [Fencer(name, i) for i, name in enumerate(names)]
-    pairs = []
-
-    max_unique_pairs = (num_fencers * (num_fencers - 1)) // 2
-    added_pairs = 0
-
-    while added_pairs < max_unique_pairs:
-        one = random.randint(0, num_fencers - 1)
-        two = random.randint(0, num_fencers - 1)
-
-        while one == two:
-            two = random.randint(0, num_fencers - 1)
-
-        already_fenced = two in fencers[one].fenced
-
-        if not already_fenced:
-            new_pair = Pair(fencers[one].id, fencers[two].id)
-            pairs.append(new_pair)
-            fencers[one].fenced.append(fencers[two].id)
-            fencers[two].fenced.append(fencers[one].id)
-            added_pairs += 1
-
-    ordered_pairs = reorder(pairs, fencers, max_unique_pairs)
-    return ordered_pairs
+    def push_to_strip(self, pair):
+        # Deletes the pair from their respective lists within the dictionary
+        self.pool[pair[0].bouts].remove(pair[0])
+        self.pool[pair[1].bouts].remove(pair[1])
+        # Advances number of bouts completed, adds each other to each fencers fenced set
+        pair[0].bouts += 1
+        pair[0].last = pair[1]
+        pair[1].bouts += 1
+        pair[1].last = pair[0]
+        # Adds pair to new corresponding lists
+        self.pool[pair[0].bouts].add(pair[0])
+        self.pool[pair[1].bouts].add(pair[1])
